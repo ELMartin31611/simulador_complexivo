5be661ab68ae8efe41a5f673fddd23d55957816b
ELMartin31611 <martinborja31611@gmail.com>
2026-08-12 18:17:22 -0500

    models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Vehicles(models.Model):
+    plate = models.CharField(max_length=10, unique=True)
+    brand = models.CharField(max_length=40, unique=True)
+    daily_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.plate
+
+  
+
+class Estado(models.TextChoices):
+        RESERVED = "reservado", "Reservado"
+        ACTIVE = "activo", "Activo"
+        CLOSED = "cerrado", "Cerrado"
+        CANCELLED = "cancelado", "Cancelado"
+
+
+class Rentals(models.Model):
+    vehicle_id = models.ForeignKey(Vehicles, on_delete=models.PROTECT, related_name="vehiculos")
+    customer_name = models.CharField(max_length=120)
+    total = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2,  
+        default=0
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=Estado.choices,
+        default=Estado.ACTIVE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.Vehicles.brand} {self.customer_name}"
